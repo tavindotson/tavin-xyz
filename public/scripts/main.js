7676572f72e392cb5661f0f0f3145ebb818baed4
Tavin Dotson <tjd@tavin.xyz>
2022-10-08 22:04:17 -0700

    adjusting for the new fullscreen code

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,4 @@
-import { addElementBody } from "./modules/util.js";
+import { addElementBody, setCookie, getCookie } from "./modules/util.js";
 import { checkUser } from "./modules/firebase.js";
 import { setTheme, themeColor } from "./modules/theme.js";
 
@@ -9,3 +9,29 @@ addElementBody("./scripts/site-events.js", "script");
 addElementBody("./scripts/modules/firebase.js", "script", "module");
 
 checkUser();
+
+function askForFullScreen() {
+  // If there is no cookie with the name QuestionFullscreen then ask the user if they would like to go fullscreen.
+  if (!getCookie("QuestionFullscreen")) {
+    if (
+      confirm(
+        "This website is better experienced in fullscreen mode. Click OK to go fullscreen."
+      )
+    ) {
+      setCookie("QuestionFullscreen", true);
+      document.documentElement.requestFullscreen();
+    } else {
+      setCookie("QuestionFullscreen", "declined");
+    }
+  }
+}
+
+if (
+  !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  )
+) {
+  askForFullScreen();
+} else {
+  setCookie("QuestionFullscreen", "autodecline-mobile");
+}
