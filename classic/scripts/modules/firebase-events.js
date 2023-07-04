69c0ee4f34b8c335b4c47f783323b19d753372da
Tavin Dotson <tjd@tavin.xyz>
2023-07-03 20:51:13 -0700

    removed all instances of foobar

--- classic/scripts/modules/firebase-events.js
+++ classic/scripts/modules/firebase-events.js
@@ -101,16 +101,17 @@ onAuthStateChanged(auth, (user) => {
 });
 
 // This file is part of the source code of Tavin.xyz.
+// This file is part of the source code of Tavin.xyz.
 
 // This is free software: you can redistribute it and / or modify it under
 // the terms of the GNU General Public License as published by the Free
-// Software Foundation, either version 3 of the License, or(at your option)
+// Software Foundation, either version 3 of the License, or (at your option)
 // any later version.
 
 // This code is distributed in the hope that it will be useful, but WITHOUT
 // ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-// FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
 // more details.
 
 // You should have received a copy of the GNU General Public License along
-// with Foobar.If not, see < https://www.gnu.org/licenses/>.
+// with this source. If not, see < https://www.gnu.org/licenses/>.
